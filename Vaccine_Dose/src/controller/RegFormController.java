56214aef8d65392b3c91f864c16944dbc27501f1
Malith H Puhulwellage <56906043+malithsumuditha@users.noreply.github.com>
2021-09-17 17:14:22 +0530

    update

--- Vaccine_Dose/src/controller/RegFormController.java
+++ Vaccine_Dose/src/controller/RegFormController.java
@@ -1,8 +1,20 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 /**
  * @author - Hw_Dulanjana
  * @date - 9/12/2021
  */
 public class RegFormController {
+    public JFXTextField txtPersonId;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContact;
+    public JFXTextField txtNIC;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+        
+    }
 }
