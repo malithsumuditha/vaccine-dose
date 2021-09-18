3ef477d39cffabe2479408eb98240f13fcfa085a
Malith H Puhulwellage <56906043+malithsumuditha@users.noreply.github.com>
2021-09-18 16:14:00 +0530

    up

--- Vaccine_Dose/src/controller/RegFormController.java
+++ Vaccine_Dose/src/controller/RegFormController.java
@@ -1,15 +1,22 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 import db.DBConnection;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import tm.ViewAllPersonsTM;
 
 import java.io.IOException;
 import java.sql.*;
@@ -30,10 +37,12 @@ public class RegFormController {
     public Label lblPersonID;
     public Label lblAllRegistered;
     public AnchorPane personRegisterPanel;
+    public TableView<ViewAllPersonsTM> tblViewPersons;
+    public JFXButton btnAdd;
 
 
     public void initialize(){
-
+        tblViewPersons.setVisible(false);
         txtName.requestFocus();
         autoGenerateID();
         countAllRegisteredPersons();
@@ -91,11 +100,11 @@ public class RegFormController {
 
     }
 
-    public void rdbFemaleOnAction(ActionEvent actionEvent) {
+    public void rdbFemaleOnAction() {
         rdbMale.setSelected(false);
     }
 
-    public void rdbMaleOnAction(ActionEvent actionEvent) {
+    public void rdbMaleOnAction() {
         rdbFemale.setSelected(false);
     }
 
@@ -178,8 +187,83 @@ public class RegFormController {
     }
 
     public void btnViewAllOnAction(ActionEvent actionEvent) throws IOException {
-       Parent parent = FXMLLoader.load(this.getClass().getResource("../view/ViewAllPersonForm.fxml"));
-       personRegisterPanel.getChildren().clear();
-       personRegisterPanel.getChildren().add(parent);
+
+        tblViewPersons.setVisible(true);
+        loadList();
+
     }
+
+    public void loadList(){
+        ObservableList<ViewAllPersonsTM> items = tblViewPersons.getItems();
+        items.clear();
+
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from person");
+
+            while (resultSet.next()){
+                String id = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                String address = resultSet.getString(3);
+                String contact = resultSet.getString(4);
+                String nic = resultSet.getString(5);
+                String gender = resultSet.getString(6);
+                String registerTime = resultSet.getString(7);
+
+                ViewAllPersonsTM viewAllPersonsTM = new ViewAllPersonsTM(id,name,address,contact,nic,gender,registerTime);
+                items.add(viewAllPersonsTM);
+            }
+            tblViewPersons.refresh();
+
+            //only for tables
+            tblViewPersons.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+            tblViewPersons.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+            tblViewPersons.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
+            tblViewPersons.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("contact"));
+            tblViewPersons.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("nic"));
+            tblViewPersons.getColumns().get(5).setCellValueFactory(new PropertyValueFactory<>("gender"));
+            tblViewPersons.getColumns().get(6).setCellValueFactory(new PropertyValueFactory<>("registerTime"));
+
+            tblViewPersons.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<ViewAllPersonsTM>() {
+                @Override
+                public void changed(ObservableValue<? extends ViewAllPersonsTM> observable, ViewAllPersonsTM oldValue, ViewAllPersonsTM newValue) {
+
+                    btnAdd.setDisable(true);
+
+                    ViewAllPersonsTM selectedItem = tblViewPersons.getSelectionModel().getSelectedItem();
+
+                    String id = selectedItem.getId();
+                    String name = selectedItem.getName();
+                    String address = selectedItem.getAddress();
+                    String contact = selectedItem.getContact();
+                    String nic = selectedItem.getNic();
+                    String gender = selectedItem.getGender();
+
+                    lblPersonID.setText(id);
+                    txtName.setText(name);
+                    txtAddress.setText(address);
+                    txtContact.setText(contact);
+                    txtNIC.setText(nic);
+                    if (gender.equals("Male")){
+                        rdbMale.setSelected(true);
+                        rdbMaleOnAction();
+                    }else {
+                        rdbFemale.setSelected(true);
+                        rdbFemaleOnAction();
+
+                    }
+
+                }
+            });
+
+
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
 }
