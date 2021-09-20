8ce7240a7566ee25bc946fa1f8bc6f16dba060ac
Malith H Puhulwellage <56906043+malithsumuditha@users.noreply.github.com>
2021-09-20 17:18:22 +0530

    up

--- Vaccine_Dose/src/controller/DashBordMainViewFormController.java
+++ Vaccine_Dose/src/controller/DashBordMainViewFormController.java
@@ -1,15 +1,22 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -30,7 +37,16 @@ public class DashBordMainViewFormController implements Initializable {
     public AnchorPane mainPanel;
     public AnchorPane changingPanel;
     public Label lblDateAndTime;
-
+    public JFXButton btnDashBord;
+    public JFXButton btnRegister;
+    public JFXButton btnVaccine;
+    public JFXButton btnVaccination;
+    public JFXButton btnPHIRegister;
+    public Separator sptDashbord;
+    public Separator sptRegister;
+    public Separator sptVaccine;
+    public Separator sptVaccination;
+    public Separator sptPHIRegister;
 
 
     @Override
@@ -119,5 +135,100 @@ public class DashBordMainViewFormController implements Initializable {
         Parent parent = FXMLLoader.load(this.getClass().getResource("../view/PHIRegForm.fxml"));
         changingPanel.getChildren().clear();
         changingPanel.getChildren().add(parent);
+    }
+
+    public void btnDashBordOnMouseClicked(MouseEvent mouseEvent) {
+        btnDashBord.setStyle("-fx-background-color: #3742fa");
+        btnPHIRegister.setStyle("-fx-background-color: #3498db");
+        btnRegister.setStyle("-fx-background-color: #3498db");
+        btnVaccination.setStyle("-fx-background-color: #3498db");
+        btnVaccine.setStyle("-fx-background-color:  #3498db");
+    }
+
+    public void btnDashBordOnMouseEntered(MouseEvent mouseEvent) {
+        onMouseEnterChange(btnDashBord,sptDashbord);
+    }
+
+    public void btnDashBordOnMouseExited(MouseEvent mouseEvent) {
+        onMouseExitChange(btnDashBord,sptDashbord);
+    }
+    
+    public void btnRegisterOnMouseClick(MouseEvent mouseEvent) {
+        btnRegister.setStyle("-fx-background-color: #3742fa");
+        btnPHIRegister.setStyle("-fx-background-color: #3498db");
+        btnDashBord.setStyle("-fx-background-color: #3498db");
+        btnVaccination.setStyle("-fx-background-color: #3498db");
+        btnVaccine.setStyle("-fx-background-color:  #3498db");
+    }
+
+    public void btnRegisterOnMouseEnter(MouseEvent mouseEvent) {
+        onMouseEnterChange(btnRegister,sptRegister);
+    }
+
+    public void btnRegisterOnMouseExit(MouseEvent mouseEvent) {
+        onMouseExitChange(btnRegister,sptRegister);
+    }
+
+    public void btnVaccineOnMouseClicked(MouseEvent mouseEvent) {
+        btnVaccine.setStyle("-fx-background-color: #3742fa");
+        btnPHIRegister.setStyle("-fx-background-color: #3498db");
+        btnDashBord.setStyle("-fx-background-color: #3498db");
+        btnVaccination.setStyle("-fx-background-color: #3498db");
+        btnRegister.setStyle("-fx-background-color:  #3498db");
+    }
+
+    public void btnVaccineOnMouseEnter(MouseEvent mouseEvent) {
+        onMouseEnterChange(btnVaccine,sptVaccine);
+    }
+
+    public void btnVaccineOnMouseExit(MouseEvent mouseEvent) {
+        onMouseExitChange(btnVaccine,sptVaccine);
+    }
+
+    public void onMouseEnterChange(JFXButton btn,Separator separator){
+        btn.setFont(Font.font(null, FontWeight.BOLD,25));
+        VBox.setMargin(separator, new Insets(0,20,0,20));
+    }
+
+    public void onMouseExitChange(JFXButton btn,Separator separator){
+        btn.setFont(Font.font(null, FontWeight.BOLD,22));
+        VBox.setMargin(separator, new Insets(0,40,0,40));
+    }
+
+    public void btnVaccinationOnMouseClicked(MouseEvent mouseEvent) {
+        btnVaccination.setStyle("-fx-background-color: #3742fa");
+        btnPHIRegister.setStyle("-fx-background-color: #3498db");
+        btnDashBord.setStyle("-fx-background-color: #3498db");
+        btnRegister.setStyle("-fx-background-color: #3498db");
+        btnVaccine.setStyle("-fx-background-color:  #3498db");
+    }
+
+    public void btnVaccinationOnMouseEnter(MouseEvent mouseEvent) {
+        onMouseEnterChange(btnVaccination,sptVaccination);
+    }
+
+    public void btnVaccinationOnMouseExit(MouseEvent mouseEvent) {
+        onMouseExitChange(btnVaccination,sptVaccination);
+    }
+
+
+    public void btnPHIRegisterOnMouseClicked(MouseEvent mouseEvent) {
+        btnPHIRegister.setStyle("-fx-background-color: #3742fa");
+        btnRegister.setStyle("-fx-background-color: #3498db");
+        btnDashBord.setStyle("-fx-background-color: #3498db");
+        btnVaccination.setStyle("-fx-background-color: #3498db");
+        btnVaccine.setStyle("-fx-background-color:  #3498db");
+    }
+
+    public void btnPHIRegisterOnMouseEnter(MouseEvent mouseEvent) {
+        onMouseEnterChange(btnPHIRegister,sptPHIRegister);
+    }
+
+
+    public void btnPHIRegisterOnMouseExit(MouseEvent mouseEvent) {
+        onMouseExitChange(btnPHIRegister,sptPHIRegister);
+    }
+
+    public void btnEditProfile(ActionEvent actionEvent) {
     }
 }
