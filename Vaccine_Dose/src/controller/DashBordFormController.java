a88f50468958f491c22ef3be36b8abc4e34594b4
Malith H Puhulwellage <56906043+malithsumuditha@users.noreply.github.com>
2021-09-24 23:27:29 +0530

    up

--- Vaccine_Dose/src/controller/DashBordFormController.java
+++ Vaccine_Dose/src/controller/DashBordFormController.java
@@ -6,7 +6,7 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.AnchorPane;
 
-import java.sql.Connection;
+import java.sql.*;
 
 /**
  * @author : MalithHP <malithsumuditha11@gmail.com>
@@ -15,25 +15,57 @@ import java.sql.Connection;
 public class DashBordFormController {
     public AnchorPane rootD;
     public LineChart<String,Number> lineChart;
+    public String count;
 
+    public void initialize(){
+       loadLineChart();
+    }
+
+
+    public String loadLineChartSQL(String dose){
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("select count(*) from vaccination where dose =?");
+//            preparedStatement.setObject(1,"dose");
+            preparedStatement.setObject(1,dose);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                count= resultSet.getString(1);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return count;
+
+    }
 
     public void btnLoadLineChartOnAction(ActionEvent actionEvent) {
+    }
 
-        Connection connection = DBConnection.getInstance().getConnection();
-//        connection.prepareStatement("select ")
+    public void loadLineChart(){
+
+        String first_dose = loadLineChartSQL("First Dose");
+        int intCout = Integer.parseInt(first_dose);
 
         lineChart.getData().clear();
 
         XYChart.Series<String,Number> series = new XYChart.Series<String, Number>();
-        series.getData().add(new XYChart.Data<String, Number>("Dec",20));
+        series.getData().add(new XYChart.Data<String, Number>("Dec",intCout));
         series.getData().add(new XYChart.Data<String, Number>("Jan",50));
         series.getData().add(new XYChart.Data<String, Number>("Feb",10));
         series.getData().add(new XYChart.Data<String, Number>("March",70));
         series.getData().add(new XYChart.Data<String, Number>("April",60));
         series.setName("First Dose Done");
 
+
+        String second_dose = loadLineChartSQL("Second Dose");
+        intCout = Integer.parseInt(second_dose);
+
         XYChart.Series<String,Number> series2 = new XYChart.Series<String, Number>();
-        series2.getData().add(new XYChart.Data<String, Number>("Dec",10));
+        series2.getData().add(new XYChart.Data<String, Number>("Dec",intCout));
         series2.getData().add(new XYChart.Data<String, Number>("Jan",20));
         series2.getData().add(new XYChart.Data<String, Number>("Feb",5));
         series2.getData().add(new XYChart.Data<String, Number>("March",25));
@@ -41,6 +73,5 @@ public class DashBordFormController {
         series2.setName("Second Dose Done");
 
         lineChart.getData().addAll(series,series2);
-
     }
 }
