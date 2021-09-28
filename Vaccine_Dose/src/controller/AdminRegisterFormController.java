8996286dd4fa75883daf17e311fbe8d65b462eb2
Malith H Puhulwellage <56906043+malithsumuditha@users.noreply.github.com>
2021-09-29 00:56:38 +0530

    update

--- Vaccine_Dose/src/controller/AdminRegisterFormController.java
+++ Vaccine_Dose/src/controller/AdminRegisterFormController.java
@@ -1,8 +1,45 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 /**
  * @author : MalithHP <malithsumuditha11@gmail.com>
  * @since : 9/28/2021
  **/
 public class AdminRegisterFormController {
+    public JFXTextField txtName;
+    public JFXRadioButton rdbMale;
+    public JFXRadioButton rdbFemale;
+    public JFXTextField txtContact;
+    public JFXTextField txtNIC;
+    public Label lblAdminID;
+    public JFXTextField txtEmail;
+    public JFXTextField txtPassword;
+    public JFXTextField txtConfirmPassword;
+    public JFXButton btnAdd;
+    public JFXButton btnUpdate;
+    public JFXButton btnDelete;
+    public JFXButton btnReset;
+
+    public void rdbMaleOnAction(ActionEvent actionEvent) {
+    }
+
+    public void rdbFemaleOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnResetOnAction(ActionEvent actionEvent) {
+    }
 }
