1792b2f3e35d66297c101147168af2cce9937c00
Malith H Puhulwellage <56906043+malithsumuditha@users.noreply.github.com>
2021-10-05 09:09:22 +0530

    update

--- Vaccine_Dose/src/controller/DoctorRegFOrmController.java
+++ Vaccine_Dose/src/controller/DoctorRegFOrmController.java
@@ -4,8 +4,14 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
+import db.DBConnection;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * @author - Hw_Dulanjana
@@ -26,13 +32,61 @@ public class DoctorRegFOrmController {
     public JFXPasswordField txtCPassword;
     public JFXButton btnAdd1;
 
+    public void initialize(){
+        PersonRegFormController.autoGenerateID(lblDoctorID,"doctor","D");
+    }
+
     public void rdbDMaleOnAction(ActionEvent actionEvent) {
+        rdbDFemale.setSelected(false);
     }
 
     public void rdbDFemaleOnAction(ActionEvent actionEvent) {
+        rdbDMale.setSelected(false);
     }
 
     public void btnDAddOnAction(ActionEvent actionEvent) {
+
+        Connection connection = DBConnection.getInstance().getConnection();
+        String id = lblDoctorID.getText();
+        String name = txtDName.getText();
+        String contact = txtDContact.getText();
+        String nic = txtDNic.getText();
+        String password = txtAccPassword.getText();
+        String cPassword = txtCPassword.getText();
+        String gender = null;
+
+        if (rdbDMale.isSelected()){
+            gender = "Male";
+        }else if (rdbDFemale.isSelected()){
+            gender = "Female";
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR,"Select Gender");
+            alert.showAndWait();
+        }
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into doctor values(?,?,?,?,?,?)");
+            preparedStatement.setObject(1,id);
+            preparedStatement.setObject(2,name);
+            preparedStatement.setObject(3,contact);
+            preparedStatement.setObject(4,nic);
+            preparedStatement.setObject(5,gender);
+            preparedStatement.setObject(6,password);
+
+            int i = preparedStatement.executeUpdate();
+
+            if (i!=0){
+                confirmAlert("Successfully Added");
+            }else {
+                errorAlert("Something Error");
+            }
+
+        } catch (SQLException throwables) {
+            errorAlert("Something Error");
+            throwables.printStackTrace();
+        }
+
+
     }
 
     public void btnUpdateOnAction(ActionEvent actionEvent) {
@@ -45,5 +99,15 @@ public class DoctorRegFOrmController {
     }
 
     public void btnFileChooserAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void confirmAlert(String displayAlert){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,displayAlert);
+        alert.showAndWait();
+    }
+
+    public void errorAlert(String displayAlert){
+        Alert alert = new Alert(Alert.AlertType.ERROR,displayAlert);
+        alert.showAndWait();
     }
 }
